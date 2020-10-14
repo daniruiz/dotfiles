058ae308048009c41d98212cb068891c4eea49a2
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-10-14 22:52:03 +0200

    Fix install script for BSD

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cp -rfv .config .local .gitconfig .bashrc .zshrc ~/
-sudo ln -rsfv ~/.bashrc /root/.bashrc
-sudo ln -rsfv ~/.zshrc /root/.zshrc
+sudo ln -sfv ~/.bashrc /root/.bashrc
+sudo ln -sfv ~/.zshrc /root/.zshrc
