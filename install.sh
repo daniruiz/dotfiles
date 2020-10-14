2333bf53c6cd329bba9f3ce54833c716ee434239
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-10-14 22:13:37 +0200

    Improve install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-cp -r .config .local .bashrc .gitconfig .zshrc ~/
+cp -rfv .config .local .gitconfig .bashrc .zshrc ~/
+sudo ln -rsfv ~/.bashrc /root/.bashrc
+sudo ln -rsfv ~/.zshrc /root/.zshrc
