e5f11275f9b2e08b5cf3a172e60b7724f9e1527a
Daniel Ruiz de Alegría <daniel@drasite.com>
2021-03-14 19:49:34 +0100

    Add qt5 themes

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 cp -rfv .config .local .gitconfig .bashrc .zshrc ~/
 sudo ln -sfv ~/.bashrc /root/.bashrc
 sudo ln -sfv ~/.zshrc /root/.zshrc
+
+echo Configure QT theme
+echo QT_QPA_PLATFORMTHEME=qt5ct | sudo tee -a /etc/environment
