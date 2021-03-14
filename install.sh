5502a708d6722756eaedbfbbbf881b063b586250
Daniel Ruiz de Alegría <daniel@drasite.com>
2021-03-14 19:49:56 +0100

    Symlink sh to dash

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 cp -rfv .config .local .gitconfig .bashrc .zshrc ~/
 sudo ln -sfv ~/.bashrc /root/.bashrc
 sudo ln -sfv ~/.zshrc /root/.zshrc
+[ -f /bin/dash ] && sudo ln -sfv /bin/sh /bin/dash
 
 echo Configure QT theme
 echo QT_QPA_PLATFORMTHEME=qt5ct | sudo tee -a /etc/environment
