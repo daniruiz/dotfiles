9d9f23d0e987fc8e346f1ab9d9dc16952bef50e9
Daniel Ruiz de Alegría <daniel@drasite.com>
2021-03-15 18:00:04 +0100

    Fix dash symlink to sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 cp -rfv .config .local .gitconfig .bashrc .zshrc ~/
 sudo ln -sfv ~/.bashrc /root/.bashrc
 sudo ln -sfv ~/.zshrc /root/.zshrc
-[ -f /bin/dash ] && sudo ln -sfv /bin/sh /bin/dash
+[ -f /bin/dash ] && sudo ln -sfv /bin/dash /bin/sh
 
 echo Configure QT theme
 echo QT_QPA_PLATFORMTHEME=qt5ct | sudo tee -a /etc/environment
