ebd6649f91c7e69bc28aae0f98ffbf40b0be3bff
Daniel Ruiz de Alegría <daniel@drasite.com>
2021-10-12 18:12:15 +0200

    Update install.sh script

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-cp -rfv .config .local .gitconfig .bashrc .zshrc ~/
-sudo ln -sfv ~/.bashrc /root/.bashrc
-sudo ln -sfv ~/.zshrc /root/.zshrc
-[ -f /bin/dash ] && sudo ln -sfv /bin/dash /bin/sh
+cp -rfv .config .local ~/
+for i in .bashrc .zshrc .gitconfig .gitattributes
+do
+  ln -rsfv $i ~/$i
+  sudo ln -rsfv $i /root/$i
+done
 
 echo Configure QT theme
-echo QT_QPA_PLATFORMTHEME=qt5ct | sudo tee -a /etc/environment
+grep -q QT_QPA_PLATFORMTHEME=qt5ct /etc/environment \
+  || echo QT_QPA_PLATFORMTHEME=qt5ct | sudo tee -a /etc/environment
 
 sudo cp -rv .local/share/qt5ct /usr/share
